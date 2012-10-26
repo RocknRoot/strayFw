bbf8f87902e5995a90a8f1a05051ed94fc543f64
Nekith <nekith@gmail.com>
2012-10-26 15:27:29 +0200

    Tests bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+$base = rtrim(dirname(__FILE__), '/') . '/';
+
+define('STRAY_PATH_TO_APPS', $base . '../apps/');
+define('STRAY_PATH_TO_MODELS', $base . '../models/');
+define('STRAY_PATH_TO_LIB', $base . '../lib/');
+define('STRAY_PATH_TO_SCRIPTS', $base . '../scripts/');
+define('STRAY_PATH_TO_WEB', $base . '../web/');
+define('STRAY_PATH_TO_INSTALL', $base . '../');
+
+// stray
+$straypath = STRAY_PATH_TO_LIB . 'stray/';
+require $straypath . 'global/require.php';
+require $straypath . 'exception/require.php';
+require $straypath . 'config/require.php';
+require $straypath . 'persistance/require.php';
+require $straypath . 'models/require.php';
+require $straypath . 'models/mod/require.php';
+require $straypath . 'models/mutation/require.php';
+require $straypath . 'models/query/require.php';
+require $straypath . 'routing/require.php';
+require $straypath . 'routing/strayRoutingBootstrap.class.php';
+require $straypath . 'locale/require.php';
+require $straypath . 'command/require.php';
+require $straypath . 'apps/require.php';
+require $straypath . 'form/require.php';
+require $straypath . 'ext/require.php';
