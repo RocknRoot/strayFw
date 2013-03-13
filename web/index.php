6dec01e71042b12f39261413a28fa17ca0cf9fc0
Valerian Cubero <nekith@gmail.com>
2013-03-13 18:25:59 +0100

    Fix web/index foreach

--- web/index.php
+++ web/index.php
@@ -15,8 +15,8 @@ if (false === function_exists('getallheaders'))
   function getallheaders()
   {
     $headers = null;
-    foreach ($_SERVER as $n => $v)
-      if ('HTTP_' == substr($n, 0, 5))
+    foreach ($_SERVER as $name => $value)
+      if ('HTTP_' == substr($name, 0, 5))
         $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
     return $headers;
   }
