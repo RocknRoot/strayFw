9b805e3c1a1d257ea13266bd9b8fb2cf32659509
Valerian Cubero <nekith@gmail.com>
2013-10-17 03:13:08 +0200

    Add PHP Coding Standards Fixer to travis

--- quality/quality.sh
+++ quality/quality.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-phpunit --configuration phpunit_pgsql.xml --coverage-text
+phpunit --configuration quality/phpunit_pgsql.xml --coverage-text
 
-php ../php-cs-fixer.phar ../ --dry-run
+php php-cs-fixer.phar . --dry-run
